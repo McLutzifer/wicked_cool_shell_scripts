7805eef65e91890cfcc899591303165da265f6ba
McLutzifer <lukasschumi@gmx.at>
2021-04-03 20:32:12 +0200

    update

--- path.sh
+++ path.sh
@@ -5,4 +5,11 @@ in_path()
 {
     cmd=$1  ourpath=$2  result=1
     oldIFS=$IFS     IFS=":"
+
+    for directory in "$ourpath"
+    do
+        if [ -x $directory/$cmd ] ; then
+            result=0    # If we're here, we found the command.
+        fi
+    done
 }
